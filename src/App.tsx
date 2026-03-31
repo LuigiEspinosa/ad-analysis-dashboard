719c63b32e3bb2b32d2cf4690e9c513043a1f567
Cuatro <luigi@cuatro.dev>
2026-03-31 15:10:37 -0500

    chore(scaffold): Initialize vite react-ts + tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white dark:bg-gray-950">
+      <p className="text-gray-900 dark:text-white p-4">Scaffold Ok</p>
+    </div>
+  );
+}
+
+export default App;
